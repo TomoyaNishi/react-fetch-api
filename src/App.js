eb0b1e8b3c2645c91124c98867015f30d65ad78d
YutamaKotaro <california.dreamin.never.die@gmail.com>
2022-06-06 21:19:51 +0900

    add jsonp

--- src/App.js
+++ src/App.js
@@ -10,30 +10,30 @@ function App() {
     navigator.geolocation.getCurrentPosition(async function (success) {
       let lat = success.coords.latitude;
       let long = success.coords.longitude;
-      // const url = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=9c3b52cc8cfb70b0&lat=${lat}&lng=${long}&range=5&format=json`;
-
-      // await fetchJsonp(url, {})
-      //   .then(function (response) {
-      //     console.log(response);
-      //     return response.json();
-      //   })
-      //   .then(function (json) {
-      //     console.log("success", json);
-      //     console.log(json);
-      //     return json;
-      //   })
-      //   .catch(function (err) {
-      //     console.log("error", err);
-      //   });
+      const url = `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=9c3b52cc8cfb70b0&lat=${lat}&lng=${long}&range=5&format=jsonp`;
+      console.log('before request')
+      await fetchJsonp(url, {})
+        .then(function (response) {
+          console.log(response);
+          return response.json();
+        })
+        .then(function (json) {
+          console.log("success", json);
+          console.log(json);
+          return json;
+        })
+        .catch(function (err) {
+          console.log("error", err);
+        });
 
       // console.log(a);
 
-      let res = await fetch(
-        `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=9c3b52cc8cfb70b0&lat=${lat}&lng=${long}&range=5&format=json`
-      );
-
-      const json = await res.json();
-      setShops(json.results.shop);
+      // let res = await fetch(
+      //   `https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=9c3b52cc8cfb70b0&lat=${lat}&lng=${long}&range=5&format=json`
+      // );
+      //
+      // const json = await res.json();
+      // setShops(json.results.shop);
     });
   }
 
